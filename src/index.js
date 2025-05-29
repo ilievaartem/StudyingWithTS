6cb5b2f367dcb247983d90bf78a5da734d187c84
Iliev Artem <aartemiliev@gmail.com>
2025-05-29 16:06:58 +0300

    Studyed with writting code in TS for

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello TS');
+// const firstName: string = 'Artem';
+// const lastName: string = 'Iliev';
+// console.log(`${firstName} ${lastName}`);
+// for(let i = 0; i < 10; i++){
+//     if(i % 2 === 0){
+//         console.log(`Парні числа з циклу for - ${i}`);
+//     }
+// };
+const form = document.querySelector('#factorial-form'), stepsList = document.querySelector('#factorial-steps');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    stepsList.textContent = ' ';
+    const numberInput = document.querySelector('#number');
+    const number = Number(numberInput.value);
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+        const listItem = document.createElement('li');
+        listItem.textContent = `${factorial}`;
+        stepsList.appendChild(listItem);
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello TS');
+// const firstName: string = 'Artem';
+// const lastName: string = 'Iliev';
+// console.log(`${firstName} ${lastName}`);
+// for(let i = 0; i < 10; i++){
+//     if(i % 2 === 0){
+//         console.log(`Парні числа з циклу for - ${i}`);
+//     }
+// };
+
+const form = document.querySelector('#factorial-form') as HTMLFormElement,
+      stepsList = document.querySelector('#factorial-steps') as HTMLOListElement;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    stepsList.textContent = ' ';
+
+    const numberInput = document.querySelector('#number') as HTMLInputElement;
+    const number: number = Number(numberInput.value);
+
+    let factorial: number = 1;
+    for(let i = 1; i <= number; i++) {
+        factorial *= i;
+
+        const listItem: HTMLLIElement = document.createElement('li');
+        listItem.textContent = `${factorial}`;
+        stepsList.appendChild(listItem);
+    }
+});
