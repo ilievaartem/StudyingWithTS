10ba22e91cb64e512003be0bd414aeed77f0ed45
Iliev Artem <aartemiliev@gmail.com>
2025-05-31 18:29:09 +0300

    Array examaples + type

--- src/index.js
+++ src/index.js
@@ -26,3 +26,49 @@ ageForm.addEventListener('submit', (e) => {
         response.textContent = `${name}, aлкоголь заборонений`;
     }
 });
+//1
+const numbers = [4, 2, 7, 1, 9, 5];
+let maxNumber = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+        maxNumber = numbers[i];
+    }
+}
+;
+//2
+const numbers2 = [10, 20, 30, 40, 50];
+let sum = 0;
+for (let num of numbers2) {
+    sum += num;
+}
+;
+const average = sum / numbers2.length;
+//3
+const numbers3 = [10, -20, 30, -40, 50];
+let firstNegative;
+for (let num of numbers3) {
+    if (num < 0) {
+        firstNegative = num;
+        break;
+    }
+}
+;
+const item = { name: 'Футболка', price: 20 };
+const discount = 10;
+const discountAmount = (discount / 100) * item.price;
+const totalCost = item.price - discountAmount;
+const employee = {
+    name: 'John',
+    hourRate: 10
+};
+const JohnAmount = employee.hourRate * 40;
+const invoice = { name: employee.name, amount: JohnAmount };
+const books = [
+    { title: 'Грокаємо алгоритми', author: 'Адіться Бхаргва', price: 25 },
+    { title: 'Мова програмування C++', author: 'Б\'єрн Страуструп', price: 20 },
+    { title: 'Гаррі Поттер і філософський камінь', author: 'Джоан Роулінг', price: 10 }
+];
+for (let book of books) {
+    console.log(`Книга: ${book.title}, автор: ${book.author}, ціна: ${book.price}$`);
+}
+;
--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,80 @@ ageForm.addEventListener('submit', (e) => {
         response.textContent = `${name}, aлкоголь заборонений`;
     }
 });
+//1
+const numbers: number[] = [4, 2, 7, 1, 9, 5];
+let maxNumber: number = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > maxNumber) {
+        maxNumber = numbers[i];
+    }
+};
+
+//2
+const numbers2: number[] = [10, 20, 30, 40, 50];
+let sum: number = 0;
+
+for (let num of numbers2) {
+    sum += num;
+};
+
+const average: number = sum / numbers2.length;
+
+//3
+const numbers3: number[] = [10, -20, 30, -40, 50];
+let firstNegative: number | undefined;
+
+for (let num of numbers3) {
+    if(num < 0) {
+        firstNegative = num;
+        break;  
+    }
+};
+
+//4
+type Item = {
+    name: string;
+    price: number;
+};
+
+const item: Item = { name: 'Футболка', price: 20 }
+
+const discount: number = 10;
+const discountAmount: number = (discount / 100) * item.price;
+const totalCost: number = item.price - discountAmount;
+
+//5
+type Employee = {
+    name: string;
+    hourRate: number;
+};
+
+type Paycheck = {
+    name: string;
+    amount: number;
+};
+
+const employee: Employee = {
+    name: 'John',
+    hourRate: 10
+};
+const JohnAmount: number = employee.hourRate * 40;
+const invoice: Paycheck = { name: employee.name, amount: JohnAmount };
+
+//6
+type Book = {
+    title: string,
+    author: string;
+    price: number
+};
+
+const books: Book[] = [
+    { title: 'Грокаємо алгоритми', author: 'Адіться Бхаргва', price: 25 },
+    { title: 'Мова програмування C++', author: 'Б\'єрн Страуструп', price: 20 },
+    { title: 'Гаррі Поттер і філософський камінь', author: 'Джоан Роулінг', price: 10 }
+];
+
+for (let book of books) {
+    console.log(`Книга: ${book.title}, автор: ${book.author}, ціна: ${book.price}$`);
+};
